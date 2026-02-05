c9faa39465c61723a5fae337f6b3a98d6c922109
engoguru <engoguru@carkverma.com>
2026-02-05 14:27:55 +0530

    05/2026-2

--- src/utils/BaseUrl.jsx
+++ src/utils/BaseUrl.jsx
@@ -1,4 +1,4 @@
-// export const base_URL="http://13.205.63.37:5000/api/"
+export const base_URL="http://13.205.63.37:5000/api/"
 
 
-export const base_URL="http://localhost:5000/api/"    
+// export const base_URL="http://localhost:5000/api/"    
